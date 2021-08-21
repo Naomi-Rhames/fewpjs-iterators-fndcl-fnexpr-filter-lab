3505d641299cfc1ad042489e2e46b97e8046c87f
Naomi-Rhames <naomirhames2020@gmail.com>
2021-08-20 19:07:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function findMatching(drivers, string){ 
-    
-    }
+    return drivers.filter(driver => driver.toUpperCase()=== string.toUpperCase())
+ }
+ function fuzzyMatch(drivers, string){
+     return drivers.filter(driver => driver.match() === string.match())
+
+ }
+
+
